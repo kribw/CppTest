6c53779a84c5d2fd9b18a27667ca646ccc87ece3
Kristoffer Berg Wilhelmsen <kwi059@uit.no>
2021-08-26 16:06:11 +0200

    Added files

---
+++ cmake-build-debug/CMakeFiles/HelloWorldClion.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloWorldClion.dir/main.cpp.obj"
+  "HelloWorldClion.exe"
+  "HelloWorldClion.exe.manifest"
+  "HelloWorldClion.lib"
+  "HelloWorldClion.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HelloWorldClion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
