2b3177a4b93cbbe824a3bb1cec9c599011304264
Kristoffer Berg Wilhelmsen <kwi059@uit.no>
2021-09-06 21:30:51 +0200

    Refactored binary_sort

--- binary_sort.h
+++ binary_sort.h
@@ -6,7 +6,9 @@
 template<std::random_access_iterator Iterator_T, std::random_access_iterator Sentinel_T,
         typename Compare_T = std::less<>>
 void binary_sort(Iterator_T begin, Sentinel_T end, Compare_T comp = Compare_T()) {
+    // Get type from iterator
     using type = typename std::iterator_traits<Iterator_T>::value_type;
+
     // Binary Insertion Sort using a multiset (BST, allows duplicate values)
     std::multiset<type, Compare_T> sorted(comp);
 
@@ -16,9 +18,8 @@ void binary_sort(Iterator_T begin, Sentinel_T end, Compare_T comp = Compare_T())
     }
 
     // Set iterator values to sorted values from BST
-    auto sorted_begin = sorted.begin();
-    for (; begin != end; begin++) {
-        *begin = *sorted_begin;
-        sorted_begin++;
+    for (auto &item : sorted) {
+        *begin = item;
+        begin++;
     }
 }
