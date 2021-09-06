656f3afbbd9c2cea31f692ff5208bc6052c4d635
Kristoffer Berg Wilhelmsen <kwi059@uit.no>
2021-09-06 20:24:44 +0200

    Added Binary Insertion Sort by using multiset

--- binary_sort.h
+++ binary_sort.h
@@ -7,23 +7,18 @@ template<std::random_access_iterator Iterator_T, std::random_access_iterator Sen
         typename Compare_T = std::less<>>
 void binary_sort(Iterator_T begin, Sentinel_T end, Compare_T comp = Compare_T()) {
     using type = typename std::iterator_traits<Iterator_T>::value_type;
+    // Binary Insertion Sort using a multiset (allows duplicate values)
+    std::multiset<type, Compare_T> sorted(comp);
 
-    struct entry {
-        entry *left;
-        entry *right;
-
-        type key;
-        type value;
-    };
-
-    std::set<entry> sorted_tree = {};
-
-    if (sorted_tree.size() == 0) {
-        sorted_tree.insert(*begin);
-        begin++;
+    // Insert values from iterator into BST (multiset)
+    for (auto i = begin; i != end; ++i) {
+        sorted.insert(*i);
     }
 
+    // Set iterator values to sorted values from BST
+    auto sorted_begin = sorted.begin();
     for (; begin != end; begin++) {
-
+        *begin = *sorted_begin;
+        sorted_begin++;
     }
 }
--- main.cpp
+++ main.cpp
@@ -7,16 +7,23 @@
 
 int main() {
     std::vector<int> my_vector = {12, 2, 3, 500, 4, 5, 2, 2, 123, 4, -51, 12, 3, 6, 9, -5};
+    std::cout << "My_vector (size " << my_vector.size() << "): ";
+    for(auto item : my_vector) {
+        std::cout << item << ", ";
+    }
+    std::cout << std::endl;
 
     auto begin = my_vector.begin();
     auto end = my_vector.end();
 
-    counting_sort(begin, end);
+    //counting_sort(begin, end);
+    //radix_sort(my_vector.begin(), my_vector.end());
+    binary_sort(begin, end);
 
+    std::cout << "After sort: ";
     for (; begin != end; begin++) {
         std::cout << *begin << ", ";
     }
-    //radix_sort(my_vector.begin(), my_vector.end());
 
     return 0;
 }
