1857cb0018a24cddaf638eaf56a3e227cd45cd01
Kristoffer Berg Wilhelmsen <kwi059@uit.no>
2021-09-06 21:09:16 +0200

    Refactored counting_sort

--- counting_sort.h
+++ counting_sort.h
@@ -6,34 +6,23 @@
 template<std::random_access_iterator Iterator_T, std::random_access_iterator Sentinel_T,
         typename Compare_T = std::less<>>
 void counting_sort(Iterator_T begin, Sentinel_T end, Compare_T comp = Compare_T()) {
+    // Get type from iterator
     using type = typename std::iterator_traits<Iterator_T>::value_type;
+
+    // Sort the items with counting sort using a map
     std::map<type, int, Compare_T> items(comp);
 
-    // Keep track of iterator steps in order to go back to the beginning
-    int iterator_steps = 0;
-    // Sort the items with counting sort using a map
-    for (; begin != end; begin++) {
-        items[*begin]++;
-        iterator_steps++;
+    // Add items to map
+    for (auto i = begin; i != end; i++) {
+        items[*i]++;
     }
 
-    // Put the sorted items into a vector
-    std::vector<type> sorted_items = {};
-    for (auto &item: items) {
-        std::cout << "key: " << item.first << " value: " << item.second << "\n";
-        while (item.second > 0) {
-            sorted_items.push_back(item.first);
-            item.second--;
-        }
-    }
-
-    // Return iterator to beginning
-    begin -= iterator_steps;
-
-    auto sorted_begin = sorted_items.begin();
     // Update the iterator with the sorted values
-    for (; begin != end ; begin++) {
-        *begin = *sorted_begin;
-        sorted_begin++;
+    for (auto &item: items) {
+        while (item.second > 0 && begin != end) {
+            *begin = item.first;
+            item.second--;
+            begin++;
+        }
     }
 }
