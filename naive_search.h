fd6abde7b9611f5aae185fa8e12e744258717e13
Kristoffer Berg Wilhelmsen <kwi059@uit.no>
2021-09-19 18:17:49 +0200

    Added some comments in naive search

--- naive_search.h
+++ naive_search.h
@@ -19,25 +19,22 @@ Iterator_T naive_search(Iterator_T begin, Sentinel_T end,
     // Count characters in sequence
     int characters = std::distance(s_begin, s_end);
 
-    // Iterate through each character
+    // Iterate through each character and check for match
     for (; begin != end; ++begin) {
+        // Found a match
         if (pred(*begin, *s_begin)) {
             int matches = 0;
 
             // Count matches in string and sequence
             for (auto it_begin = begin, it_s_begin = s_begin;
-                 it_s_begin != s_end; ++it_s_begin, ++it_begin) {
-
-                if (it_begin == end) {
-                    break;
-                }
-
+                 it_s_begin != s_end && it_begin != end; ++it_s_begin, ++it_begin) {
+                // Mismatch, cancel loop and move on to next char in "begin" iterator
                 if (!pred(*it_begin, *it_s_begin)) {
                     break;
                 }
                 matches++;
             }
-            // If matches == characters in string, we have a match
+            // If matches equal to characters in string, we have a match
             // return current position of match
             if (matches == characters) {
                 return begin;
